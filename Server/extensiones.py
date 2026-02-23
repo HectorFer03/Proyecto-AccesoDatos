88e3993f35b47b0205a1f154dee53ab166a3325d
HectorFer <hector.fernandez@alumnojoyfe.iepgroup.es>
2026-02-23 15:58:37 +0100

    Actsalizacion

--- Server/extensiones.py
+++ Server/extensiones.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_pymongo import PyMongo
 
 # Inicializamos las herramientas vacías
 db = SQLAlchemy()
 jwt = JWTManager()
+mongo = PyMongo()
